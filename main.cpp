c71704358d80b98f696d3b113669daa858f5035c
Lodigolden <adamhagengruber@gmail.com>
2022-04-20 10:39:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,20 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "board.h"
-#include "piece.h"
+#include "token.h"
 
 using std::cout;
 using std::endl;
 
-         // Setting the pieces to their position on each square of the board
+// Setting the pieces to their position on each square of the board
 void Setting(sf::RenderWindow& window, Ball* BluePieces, Ball* WhitePieces)
 {
-                 // For the color white
-	
+	// For the color white
+
 	int k = 0;
-	for (int i = 0;i < 3;i++)
-	{    
-		// We used the (!) to make sure that the position of the white pieces differ from the blue pieces
-		
-		for (int j = (int)!(i % 2);j < 8;j += 2)
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = (int)!(i % 2 & 1); j < 8; j += 2)
 		{
 			WhitePieces[k].isAlive = true;
 			WhitePieces[k].x = j;
@@ -27,13 +25,13 @@ void Setting(sf::RenderWindow& window, Ball* BluePieces, Ball* WhitePieces)
 		}
 
 	}
-	
-	      // For the color blue
+
+	// For the color blue
 
 	k = 0;
-	for (int i = 0;i < 3;i++)
+	for (int i = 0; i < 3; i++)
 	{
-		for (int j = (int)(i % 2);j < 8;j += 2)
+		for (int j = (int)(i % 2 & 1); j < 8; j += 2)
 		{
 			BluePieces[k].isAlive = true;
 			BluePieces[k].x = j;
@@ -48,17 +46,18 @@ void Setting(sf::RenderWindow& window, Ball* BluePieces, Ball* WhitePieces)
 
 int main(void)
 {
-	// Initialize board: 
+	// Initialize variables: 
 	sf::RenderWindow window(sf::VideoMode(400, 400), "Checkers!");
 
-	Tile board[8][8];
+	Board gameBoard; 
 
 	sf::Color redTile = sf::Color::Red;
 	sf::Color blackTile = sf::Color::Black;
 
-       
-	   // Initialize pieces
-	
+	double xPos = 0.0, yPos = 0.0;
+
+	bool occupiedTest = false; 
+
 	Ball BluePieces[12];
 	Ball WhitePieces[12];
 
@@ -68,97 +67,70 @@ int main(void)
 		WhitePieces[i].color = sf::Color::White;
 	}
 
-	     //  Placement of the pieces on the board
+	//  Placement of the pieces on the board
 	Setting(window, BluePieces, WhitePieces);
-	
-	
-	// Initialize variables: 
-	double xTilePos = 0.0, yTilePos = 0.0;
-
-	// Construct board: 
-	for (int i = 0; i < 8; ++i)
-	{
-		xTilePos = 0.0;
-		for (int j = 0; j < 8; ++j)
-		{
-			if (i % 2 == 0)
-			{
-				if (j % 2 == 0)
-				{
-					board[j][i].setColor(redTile); 
-				}
-
-				else
-				{
-					board[j][i].setColor(blackTile); 
-				}
-			}
-
-			else
-			{
-				if (j % 2 == 0)
-				{
-					board[j][i].setColor(blackTile); 
-				}
-
-				else
-				{
-					board[j][i].setColor(redTile); 
-				}
-			}
-
-			board[j][i].setPosition(xTilePos, yTilePos); 
-			xTilePos += 50; 
-		}
-
-		yTilePos += 50; 
-
-	}
-	 
-
-	double xPos = 0.0, yPos = 0.0; 
 
 	while (window.isOpen())
 	{
-		sf::Event event; 
+		sf::Event event;
 
 		// Check if window needs to be closed: 
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 			{
-				window.close(); 
+				window.close();
 			}
 		}
 
-		// Detection of click: 
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-		{
-			xPos = sf::Mouse::getPosition(window).x; 
-			yPos = sf::Mouse::getPosition(window).y; 
-
-			cout << xPos << " " <<  yPos << endl;
-		}
-
 		// Window commands: 
-		window.clear(); 
-		
-		// Draw board:
+		window.clear();
+
+		//// Draw board:
 		for (int i = 0; i < 8; ++i)
 		{
 			for (int j = 0; j < 8; ++j)
 			{
-				window.draw(board[j][i]); 
+				window.draw(gameBoard.getTile(i, j));
 			}
 		}
-		
-		  // Draw pieces
+
+		// Draw pieces
 		for (int i = 0; i < 12; i++)
 		{
 			WhitePieces[i].Draw(window);
 			BluePieces[i].Draw(window);
 		}
 
-		window.display(); 
+		// Detection of click: 
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			xPos = sf::Mouse::getPosition(window).x;
+			yPos = sf::Mouse::getPosition(window).y;
+
+			int vectRow = 0, vectCol = 0; 
+
+			// Get matrix locations: 
+			vectCol = xPos / 50; 
+			vectRow = yPos / 50; 
+
+			cout << vectRow << " " << vectCol << endl; 
+
+			occupiedTest = gameBoard.getOccupied(vectRow, vectCol); 
+
+			if (occupiedTest == true)
+			{
+				cout << "Occupied!" << endl; 
+			}
+
+			else if(occupiedTest == false)
+			{
+				cout << "Not Occupied!" << endl; 
+			}
+
+
+		}
+
+		window.display();
 	}
 }
