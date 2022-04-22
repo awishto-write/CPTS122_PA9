c606a27ecff8ef71e5bf0191f58c10e234790e29
awishtowrite <96355681+awishtowrite@users.noreply.github.com>
2022-04-22 12:12:41 -0700

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -11,7 +11,6 @@ Menu::Menu(float width, float height)
 	            // Play option
 
 	mainMenu[0].setFont(font);
-	//mainMenu[0].setColor(sf::Color::Blue);
 	mainMenu[0].setFillColor(sf::Color::Blue);
 	mainMenu[0].setString("PLAY");
 	mainMenu[0].setCharacterSize(45);
@@ -20,8 +19,7 @@ Menu::Menu(float width, float height)
 	           // Instructions option
 	      
 	mainMenu[1].setFont(font);
-	//mainMenu[1].setColor(sf::Color::White);
-	mainMenu[1].setFillColor(sf::Color::Cyan);  // was white before
+	mainMenu[1].setFillColor(sf::Color::Cyan);  
 	mainMenu[1].setString("INSTRUCTIONS");
 	mainMenu[1].setCharacterSize(45);
 	//mainMenu[1].setPosition(sf::Vector2f(width / 2, height / (Max_main_menu + 1) * 2));
@@ -30,8 +28,7 @@ Menu::Menu(float width, float height)
 	          // Exit option
 
 	mainMenu[2].setFont(font);
-	//mainMenu[2].setColor(sf::Color::White);
-	mainMenu[2].setFillColor(sf::Color::Cyan);  // was white before
+	mainMenu[2].setFillColor(sf::Color::Cyan);  
 	mainMenu[2].setString("EXIT");
 	mainMenu[2].setCharacterSize(45);
 	mainMenu[2].setPosition(sf::Vector2f(width / 2 - 80 , height / (Max_main_menu + 1) * 3));
@@ -56,36 +53,36 @@ void Menu::draw(sf::RenderWindow& window)
 
 void Menu::moveDown()
 {
-	if (mainMenuSelected + 1 <= Max_main_menu)
+	if (mainMenuSelected + 1 <= Max_main_menu)   //Check if not on the last option (exit)
 	{
-		mainMenu[mainMenuSelected].setFillColor(sf::Color::Cyan);  // starting color
+		mainMenu[mainMenuSelected].setFillColor(sf::Color::Cyan);  // Change the previous option's color
 
-		mainMenuSelected++;
+		mainMenuSelected++;   // move to the lower option
 
 		if (mainMenuSelected == 3)
 		{
 			mainMenuSelected = 0;
 		}
 
-		mainMenu[mainMenuSelected].setFillColor(sf::Color::Blue);   // element has been picked
+		mainMenu[mainMenuSelected].setFillColor(sf::Color::Blue);   // change the new option color
 	}
 }
 
 void Menu::moveUp()
 {
-	if (mainMenuSelected - 1 <= -1)
+	if (mainMenuSelected - 1 <= -1)                  // Check if not on the first option (play)
 	{
 	
-		mainMenu[mainMenuSelected].setFillColor(sf::Color::Cyan);   // starting color
+		mainMenu[mainMenuSelected].setFillColor(sf::Color::Cyan);    // Change the previous option's color
 
-		mainMenuSelected--;
+		mainMenuSelected--;   // move to the upper option
 
 		if (mainMenuSelected == -1)
 		{
 			mainMenuSelected = 2;
 		}
 
-		mainMenu[mainMenuSelected].setFillColor(sf::Color::Blue); // element has been picked
+		mainMenu[mainMenuSelected].setFillColor(sf::Color::Blue);   // change the new option color
 	}
 }
 
@@ -131,12 +128,12 @@ void Menu::instructions_text(sf::RenderWindow& window_play)
 	text.setString("the movement rules described below.After you move one checker, your turn is over.
 	text.setString("	The game continues with players alternating turns.
 		Movement Rules
-		• Always move your checker diagonally forward, toward your opponent's side of the gameboard).
+		â€¢ Always move your checker diagonally forward, toward your opponent's side of the gameboard).
 		Note: After a checker becomes a "King," it can move diagonally forward or backward.
-		• Move your checker one space diagonally, to an open adjacent square; or jump one or more checkers
+		â€¢ Move your checker one space diagonally, to an open adjacent square; or jump one or more checkers
 		diagonally to an open square adjacent to the checker you jumped.When you jump over an opponent's checker, 
 		you capture it(see Capturing an Opponent's Checker).
-			• If all squares adjacent to your checker are occupied, your checker is blockedand cannot move.
+			â€¢ If all squares adjacent to your checker are occupied, your checker is blockedand cannot move.
 			Capturing an Opponent's Checker If you jump an opponent's checker, you capture it.
 			Remove it from the gameboardand place it in front of you.
 			Becoming a "King"
